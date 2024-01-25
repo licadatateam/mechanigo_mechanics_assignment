1d1ab555f92e8b8c39a5adf8f862cf3118b84ce4
LICA Data Team <datateam.licagroup@gmail.com>
2024-01-25 11:38:49 +0800

    Add files via upload
    - config: regenerated redash api key
    - barangay_processing: added module
    - mgo_assign_opt_main: added address cleaning and geocoding

---
+++ barangay_processing.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  6 09:16:14 2023
+
+@author: Arvin Jay
+"""
+
+from py_functions.gmaps_maps import Geocoder
+import pandas as pd
+
+
+
+def url(sheet_id,tab):
+    """
+    Generate a Google Sheets URL for a given sheet_id and tab.
+
+    Args:
+        sheet_id (str): The Google Sheets document's ID.
+        tab (str): The specific sheet's ID (gid).
+
+    Returns:
+        str: The URL to download the sheet as a CSV.
+    """
+    return f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?gid={tab}&format=csv"
+
+def load_sheet(sheet_info):
+    """
+    Load data from a Google Sheets document.
+
+    Args:
+        sheet_info (dict): A dictionary containing 'sheet_id' and 'tab'.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing the loaded data.
+    """
+    return pd.read_csv(url(sheet_info['sheet_id'],sheet_info['tab']))
+
+def geocode_df(df):
+    """
+    Geocode addresses in a DataFrame using the Geocoder library.
+
+    Args:
+        df (pd.DataFrame): A DataFrame containing addresses to geocode.
+
+    Returns:
+        pd.DataFrame: The input DataFrame with additional geocoding columns.
+        pd.DataFrame: A DataFrame containing geocoding review information.
+    """
+    gmaps = Geocoder()   
+    gmaps.initialize_driver()
+    df, df_review = gmaps.analyze_set(df,'address','address')
+    gmaps.close_driver()
+    return df, df_review
+
+def gather_data(barangays, orders):
+    """
+    Load data for barangays and orders from Google Sheets.
+
+    Args:
+        barangays (dict): A dictionary containing 'sheet_id' and 'tab' for barangays data.
+        orders (dict or None): A dictionary containing 'sheet_id' and 'tab' for orders data, or None.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing barangay data.
+        pd.DataFrame or None: A DataFrame containing orders data, or None if orders is not provided.
+    """
+    
+    # barangays_sheed_id = "1C4XVFaYPCMPtRFE5X_sHO_a89QCmU2bVzmnMTWwrkN8"
+    # barangays_tab = '1054857011'
+    # barangays = {'sheet_id': barangays_sheed_id,
+    #              'tab': barangays_tab}
+    # orders_sheet_id = "1C4XVFaYPCMPtRFE5X_sHO_a89QCmU2bVzmnMTWwrkN8"
+    # orders_tab = '1417590793'
+    # orders = {'sheet_id': orders_sheet_id,
+    #           'tab': orders_tab}
+    
+    df_barangays = load_sheet(barangays)
+    try:
+        df_orders = load_sheet(orders)
+    except:
+        df_orders = orders
+    return df_barangays,df_orders
+
+def get_barangay_coords(df_barangays,df_orders):
+    """
+    Merge barangays and orders DataFrames based on 'barangay' and 'municipality' columns.
+
+    Args:
+        df_barangays (pd.DataFrame): A DataFrame containing barangays data.
+        df_orders (pd.DataFrame): A DataFrame containing orders data.
+
+    Returns:
+        pd.DataFrame: A DataFrame with coordinates and merged data.
+    """
+    barangay_columns = ['barangay','municipality','lat','long']
+    df = df_orders.merge(df_barangays[barangay_columns], on=['barangay','municipality'],how='left')
+    return df
+
+def fill_table(df):
+    """
+    Fill missing 'lat' and 'long' values in a DataFrame by geocoding missing addresses.
+
+    Args:
+        df (pd.DataFrame): A DataFrame with 'lat' and 'long' columns.
+
+    Returns:
+        pd.DataFrame: The updated DataFrame with filled coordinates.
+        pd.DataFrame: A DataFrame containing geocoding review information.
+        str: An error message if an error occurs during geocoding.
+    """
+    error_message = ''
+    df_review = pd.DataFrame()
+    try:
+        df_search,df_review = geocode_df(df.loc[df['lat'].isnull()])
+        print("### problem solved ###")
+        df.update(df_search,overwrite=False)
+    except Exception as e:
+        print("### problem NOT solved ###")
+        error_message = f"Cannot complete table: {str(e)}"
+    return df, df_review, error_message
+        
+def geocode_by_barangay(barangays,orders=None):
+    """
+    Perform geocoding for barangays and orders data.
+
+    Args:
+        barangays (dict): A dictionary containing 'sheet_id' and 'tab' for barangays data.
+        orders (dict or None): A dictionary containing 'sheet_id' and 'tab' for orders data, or None.
+
+    Returns:
+        pd.DataFrame: A DataFrame with coordinates and merged data.
+        pd.DataFrame: A DataFrame containing geocoding review information.
+        str: An error message if an error occurs during geocoding.
+    """
+    df,df_review,error_message = pd.DataFrame(), pd.DataFrame(), ''
+    df_barangays,df_orders = gather_data(barangays, orders)
+    df = get_barangay_coords(df_barangays,df_orders)
+    if len(df['lat'].isnull()):
+        df,df_review,error_message = fill_table(df)
+    df = df.drop_duplicates().reset_index(drop=True)
+    return df,df_review,error_message
+
+# """
+# Usage:
+# df,df_review,error_message = geocode_by_barangay(barangays,orders)
+# """
--- mgo_assign_opt_main.py
+++ mgo_assign_opt_main.py
@@ -13,8 +13,10 @@ import regex as re
 import logging
 import json
 from fuzzywuzzy import fuzz, process
-
 import streamlit as st
+
+# custom modules
+from barangay_processing import geocode_by_barangay
 
 logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(message)s',
                     datefmt = '%d-%b-%y %H:%M:%S',
@@ -129,7 +131,7 @@ def extract_hub(entry: str):
 
 @st.cache_data
 def gather_data(key: str, 
-                selected_date: date) -> pd.DataFrame:
+                selected_date: date or str) -> pd.DataFrame:
     """
     Gather appointment data from the specified key and selected date.
 
@@ -162,10 +164,10 @@ def gather_data(key: str,
         appointments['province'] = appointments['province'].str.title()
         # filter appointments on date
         try:
-            appointments = appointments.loc[appointments['date']==selected_date].drop(columns = ['date'])
-        except:
+            # assume type str, if date, trigger exception
             appointments = appointments.loc[appointments['date']==selected_date.strftime('%Y-%m-%d')].drop(columns = ['date'])
-      
+        except:
+            appointments = appointments.loc[appointments['date']==selected_date].drop(columns = ['date'])
         # drop duplicates
         appointments = appointments.drop_duplicates(subset = ['appointment_id']).reset_index(drop = True)
         appointments['address'] = appointments['address'].fillna('')
@@ -177,6 +179,152 @@ def gather_data(key: str,
         raise e
         
     return appointments
+
+@st.cache_data
+def clean_address(row: pd.Series, 
+                  address_col: str = 'pin_address', 
+                  barangay_col: str = 'barangay',
+                  municipality_col: str = 'municipality', 
+                  province_col: str = 'province') -> tuple[str, str, str, str, str]:
+    """
+    Clean and format the address information in a given row of a DataFrame.
+
+    Args:
+    -----
+        - row : pd.Series
+            The row containing address-related columns.
+        - address_col : str (Optional)
+            Column name for the address. Defaults to 'pin_address'.
+        - barangay_col : str (Optional)
+            Column name for the barangay. Defaults to 'barangay'.
+        - municipality_col : str (Optional)
+            Column name for the municipality. Defaults to 'municipality'.
+        - province_col : str (Optional)
+            Column name for the province. Defaults to 'province'.
+
+    Returns:
+        - Tuple[str, str, str, str, str]: A tuple containing the cleaned street address, street name, cleaned full address,
+        full address with street name, and full address with street name and barangay.
+
+    Examples:
+        >>> import pandas as pd
+        >>> data = {'pin_address': '123 Main St, Brgy. Example, Municipality A, Province X', 'barangay': 'Example', 'municipality': 'Municipality A', 'province': 'Province X'}
+        >>> row = pd.Series(data)
+        >>> clean_address(row)
+        ('123 Main St', 'Main St', '123 Main St in Example, Municipality A, Province X in Philippines', 'Main St in Brgy. Example, Municipality A, Province X in Philippines', 'Main St in Example, Municipality A, Province X in Philippines')
+    """
+    address = row[address_col] if row[address_col] else ''
+    barangay = row[barangay_col] if row[barangay_col] else ''
+    municipality = row[municipality_col] if row[municipality_col] else ''
+    province = row[province_col] if row[province_col] else ''
+
+    # Additional logic to handle specific cases
+    if province.lower() == 'metro manila' and municipality.lower() == 'quezon':
+        municipality = 'quezon city'
+
+    remove_pattern = re.compile(r'b\.?f\.? homes')
+
+    if len(str(address)) != 0:
+        address = re.sub(remove_pattern, '', str(address))
+    
+    try:
+        if barangay in address:
+            street_address = address.split(barangay)[0].strip()
+        else:
+            if municipality in address:
+                street_address = address.split(municipality)[0].strip()
+            else:
+                if province in address:
+                    street_address = address.split(municipality)[0].strip()
+                else:
+                    street_address = address
+                    
+        cleaned_address_ = street_address
+        if street_address != address:
+            cleaned_address = ' '.join([street_address, 'in', barangay + ',', municipality + ',', province, 'in Philippines'])
+        else:
+            cleaned_address = street_address + ' in Philippines'
+    except:
+        street_address = address
+        cleaned_address_ = address
+        cleaned_address = address
+
+    street = ''
+    # Street pattern
+    st_pattern = r'(\b\w+\s+(st\.?|street)\b)'
+    match = re.search(st_pattern, street_address, re.IGNORECASE)
+    if match:
+        street = match.group(1)
+    cleaned_address_ = re.sub(street, '', cleaned_address_).strip()
+
+    try:
+        street_address_ = ' '.join([street, 'brgy.', barangay + ',', municipality + ',', province, 'in Philippines'])
+    except:
+        street_address_ = street_address
+
+    return street_address, street, cleaned_address, cleaned_address_, street_address_
+
+@st.cache_data
+def clean_address_appts(appointments : pd.DataFrame) -> pd.DataFrame:
+    '''
+    Wrapper function for clean_address
+    
+    Args:
+    -----
+        - appointments : pd.DataFrame
+            appointments data
+    
+    Returns:
+    --------
+        - _appointments : pd.DataFrame
+            copy of appointments data with cleaned address (if no errors)
+    '''
+    _appointments = appointments.copy()
+    
+    try:
+        _appointments[['street_address', 'street', 'address_query', 'partial_address',
+                      'street_address_query']] = _appointments.apply(clean_address, 
+                                                                    axis=1, 
+                                                                    result_type='expand')
+        logging.debug('Cleaning appointments addresses.')
+    except Exception as e:
+        logging.exception(e)
+    
+    return _appointments
+
+@st.cache_data
+def geocode(barangays: dict, 
+            appointments: pd.DataFrame) -> dict:
+    """
+    Perform geocoding based on barangays and appointments data.
+
+    Args:
+    -----
+        - barangays : dict
+            Dictionary containing information about barangays, typically loaded from configuration.
+        - appointments : pd.DataFrame
+            DataFrame containing appointment data.
+
+    Returns:
+    --------
+        - geo_dict : dict {pd.DataFrame, pd.DataFrame, Optional[str]}
+            A dict containing geocoded appointments DataFrame, a review DataFrame,
+        and an error message if any.
+    """
+    try:
+        _ = geocode_by_barangay(barangays, appointments)
+        geo_dict = {'appointments' : _[0],
+                    'review' : _[1],
+                    'error' : _[2]}
+        
+        logging.debug('Geocoding addresses.')
+        return geo_dict
+    
+    except Exception as e:
+        if check_streamlit():
+            st.error('FAILED: Geocoding addresses.')
+        else:
+            raise e
 
 def no_st_main():
     # configuration settings
@@ -198,6 +346,13 @@ def no_st_main():
     else:
         logging.debug(f'Found {len(appointments)} scheduled appointments for {selected_date}.')
     
+    # clean address
+    appointments = clean_address_appts(appointments)
+
+    # geocoding
+    geo_dict = geocode(config_dict['barangays'], appointments)
+    appointments = geo_dict['appointments']
+
     return appointments
     
 def st_main():
@@ -236,7 +391,13 @@ def st_main():
             st.header("Imported cleaned appointments data.")
             st.write(f'Found {len(appointments)} scheduled appointments for {selected_date}.')
             st.write(appointments)
+        
+        # clean address
+        appointments = clean_address_appts(appointments)
     
+        # geocoding
+        geo_dict = geocode(config_dict['barangays'], appointments)
+        appointments = geo_dict['appointments']
     
     return appointments
 
@@ -249,6 +410,7 @@ if __name__ == "__main__":
     st_running = check_streamlit()
     
     if st_running:
+        logging.debug('Running script in streamlit.')
         res = st_main()
     else:
         res = no_st_main()
